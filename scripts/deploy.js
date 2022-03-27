ffc1424388bf63bb8e13a9bb14f77c529a1cb4ca
spandan114 <spandanj685@gmail.com>
2022-03-27 21:58:33 +0530

    Crowdfunding project setup

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  // We get the contract to deploy
+  const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
+  const crowdfunding = await Crowdfunding.deploy();
+
+  await crowdfunding.deployed();
+
+  console.log("Crowdfunding deployed to:", crowdfunding.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

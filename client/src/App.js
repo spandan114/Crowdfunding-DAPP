a24d5f9dd8088d3c595be25c76eb8bc0c9e8fb97
spandan114 <spandanj685@gmail.com>
2022-04-08 23:56:58 +0530

    Fundraising card UI completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div >
       <Navbar/>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Dashboard/>
     </div>
   );
 }
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+var projects = [1, 2, 3];
+
+const Dashboard = () => {
+  return (
+    <div className="px-2 py-6 flex flex-col lg:px-12 lg:flex-row ">
+      <div className="lg:w-7/12 my-2 lg:my-0 lg:mx-2">
+        {projects.map((data, i) => (
+          <div className="card relative overflow-hidden my-2">
+            <div className="ribbon bg-emerald-500">Popular</div>
+            <h1 className="font-sans text-xl text-gray font-semibold hover:text-sky-500 hover:cursor-pointer">
+              Project title
+            </h1>
+            <p className="font-sans text-sm text-stone-800 tracking-tight">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+            <div className="flex flex-col lg:flex-row">
+              <div className="inner-card w-full lg:w-2/5">
+                <p className="text-md font-bold font-sans text-gray">
+                  Targeted contribution
+                </p>
+                <p className="text-sm font-bold font-sans text-gray-600 ">
+                  100000 ETH
+                </p>
+                <p className="text-md font-bold font-sans text-gray">
+                  Deadline
+                </p>
+                <p className="text-sm font-bold font-sans text-gray-600 ">
+                  2022-06-17 10:15 PM
+                </p>
+              </div>
+              <div className="inner-card w-full lg:w-3/5">
+                <label className="text-sm text-gray-700 font-semibold">
+                  Contribution amount :{" "}
+                </label>
+                <div className="flex flex-row">
+                  <input
+                    type="number"
+                    placeholder="Type here"
+                    className="input"
+                  />
+                  <button className="button">Contribute</button>
+                </div>
+                <p className="text-sm text-red-600">
+                  <span className="font-bold">NOTE : </span> Minimum
+                  contribution is 2 ETH
+                </p>
+              </div>
+            </div>
+
+            <div className="w-full bg-gray-200 rounded-full">
+              <div className="progress" style={{ width: "25%" }}>
+                25%
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="card lg:w-5/12 h-fit">
+          <p>Form</p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;

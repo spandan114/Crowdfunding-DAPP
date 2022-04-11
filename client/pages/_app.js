c9606f728a160b30ba137e09e9f1ff4a1c14bd7f
spandan114 <spandanj685@gmail.com>
2022-04-11 21:45:21 +0530

    Connect with metamask wallet

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import {wrapper} from '../redux/store'
+import { Web3ReactProvider } from '@web3-react/core'
+import Web3 from 'web3'
+
+function getLibrary(provider) {
+  return new Web3(provider)
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default wrapper.withRedux(MyApp)
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import {useEffect} from 'react';
+import { useWeb3React } from "@web3-react/core"
+import { injected } from "../helper/walletConnector"
+import { useRouter } from 'next/router';
 
 export default function Home() {
+
+  const router = useRouter()
+  const { active, account, activate, deactivate } = useWeb3React()
+
+  async function connect() {
+    try {
+      await activate(injected)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+
+  useEffect(() => {
+    if(account){
+    console.log(account)
+    router.push("/dashboard")
+    }
+  }, [account])
+  
+
+  // async function disconnect() {
+  //   try {
+  //     deactivate()
+  //   } catch (ex) {
+  //     console.log(ex)
+  //   }
+  // }
+
   return (
-    <h1>hii</h1>
+    <div className="flex flex-col items-center justify-center my-40">
+    <button onClick={connect} className="p-4 my-10 text-lg font-bold text-white rounded-md w-56 bg-[#8D8DAA] drop-shadow-md hover:bg-[#b1b1d6] hover:drop-shadow-xl">Connect to MetaMask</button>
+    {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+
+  </div>
   )
 }
